730a24460f0ab0e9313a92c33153344d24bdf7bf
unknown <shresthakishor003@gmail.com>
2026-06-15 21:49:21 +0545

    Register Page Modified

--- src/CreateAccount.jsx
+++ src/CreateAccount.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CreateAccount = () => {
   return (
     <div className="bg-[#1c1c24] min-h-screen flex items-center justify-center p-4">
-      <div className="w-full max-w-44 bg-[#13131a] rounded-3xl">
+      <div className="w-full max-w-300 flex bg-[#13131a] rounded-3xl">
       {/* <div className="w-full max-w-[1200px] bg-[#13131a] rounded-3xl overflow-hidden shadow-2xl flex flex-col md:flex-row"> */}
         {/* Left Section */}
         <div className="w-full md:w-1/2 relative">
